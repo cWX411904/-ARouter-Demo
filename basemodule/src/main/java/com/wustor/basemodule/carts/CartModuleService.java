7774c12d74d5aaeb8523034b10262563487c3242
chengkai <520726>
2019-09-04 13:19:13 +0800

    改造后的代码

---
+++ basemodule/src/main/java/com/wustor/basemodule/carts/CartModuleService.java
@@ -0,0 +1,7 @@
+package com.wustor.basemodule.carts;
+
+import com.alibaba.android.arouter.facade.template.IProvider;
+public interface CartModuleService extends IProvider {
+
+    void getCartAmount();
+}
